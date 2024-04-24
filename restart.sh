7477d8651481bc52beae397ac794a2bb99be6e08
Halil Bilir <hbilir@gmail.com>
2024-04-25 00:34:19 +0300

    Add https support to ffmpeg in Dockerfile.gpu

--- restart.sh
+++ restart.sh
@@ -2,4 +2,5 @@
 
 docker build -t whisper-asr-webservice .
 docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
 docker run -d -p 9001:9000 -v ~/.cache/whisper:/root/.cache/whisper -e ASR_MODEL=tiny -e ASR_ENGINE=faster_whisper whisper-asr-webservice
